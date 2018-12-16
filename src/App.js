f33182a14f2d9ce4a471d1a8fe14e51814ccc327
Harry Soer <gelo.soer@gmail.com>
2018-12-17 01:36:11 +0800

    Added infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
@@ -7,34 +7,62 @@ import ProductCard from './components/ProductCard';
 const ProductsSection = styled.div`
     display: flex;
     flex-wrap: wrap;
+    justify-content: space-evenly;
 `
 export default class App extends Component {
 
     state = {
         products: [],
+        isEnd: false,
+        isLoading: false,
         query: {
-            "_page": 0,
+            "_page": 1,
             "_sort": '',
             "_limit": 40
         }
     }
 
     componentDidMount() {
-        this.fetch()
+        this.fetch(this.state.query);
+
+        window.addEventListener("scroll", () => {
+            const { isLoading, isEnd, query } = this.state;
+            let newQuery = { ...query };
+    
+            if (document.documentElement) {
+                const fullHeight = document.body.offsetHeight,
+                    heightTop = window.pageYOffset,
+                    innerHeight = window.innerHeight;
+    
+                if (heightTop + innerHeight > fullHeight) {
+                    if (!isLoading && !isEnd) {
+                        newQuery = { ...newQuery, page: ++newQuery["_page"] }
+                        this.fetch(newQuery)
+                    }
+                }
+            }
+        });
     }
 
-    fetch = async (page, sortKey) => {
-        const { query, products } = this.state
+    fetch = async (query) => {
+        const { products } = this.state;
+
         const param = Object.keys(query).map((key) => {
             return encodeURIComponent(key) + '=' + encodeURIComponent(query[key])
         }).join('&')
 
+        this.setState({ isLoading: true })
+
         try {
             const { data } = await axios.get(`http://localhost:3000/api/products?${param}`)
             this.setState({
-                products: [...products, ...data]
+                products: [...products, ...data],
+                isLoading: false,
+                isEnd: data.length ? false : true,
+                query: query
             })
         } catch (err) {
+            this.setState({ isLoading: false })
             console.error(err)
         }
     }
@@ -48,15 +76,15 @@ export default class App extends Component {
                     <p>Here you're sure to find a bargain on some of the finest ascii available to purchase. Be sure to peruse our selection of ascii faces in an exciting range of sizes and prices.</p>
                 </header>
 
-                <ProductsSection id="">
+                <ProductsSection id="product-list">
                     {products.map((product, index) =>
-                        <>
-                            <ProductCard key={product.id} data={product} />
-                            {((index + 1) % 20 === 0) && ((<div style={{ width: '100%' }}>
+                        <Fragment key={product.id}>
+                            <ProductCard data={product} />
+                            {((index + 1) % 20 === 0) && ((<div style={{ width: '100%', height: '255px', textAlign: 'center' }}>
                                 <p>But first, a word from our sponsors:</p>
                                 <img class="ad" src={`/ads/?r=${Math.floor(Math.random() * 1000)}`} />
                             </div>))}
-                        </>
+                        </Fragment>
                     )}
                 </ProductsSection>
             </>)
