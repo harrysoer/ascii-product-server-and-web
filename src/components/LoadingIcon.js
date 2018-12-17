da8998ec48bd1fca47e73aa2cb631213730c85c6
Harry Soer <gelo.soer@gmail.com>
2018-12-17 23:49:28 +0800

    Created a loading component

---
+++ src/components/LoadingIcon.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export default class LoadingIcon extends Component {
+    intervalQueue = 0;
+    timeoutQueue = 0;
+    state = {
+        loadingIcon: ''
+    };
+
+    componentDidMount() {
+        clearInterval(this.intervalQueue);
+        let index = 0;
+        const frequency = 5;
+        const ASCIIs = ["–", "/", "|", "\\\\"];
+        this.intervalQueue = setInterval(() => {
+            this.timeoutQueue = setTimeout(() => {
+                this.setState({
+                    loadingIcon: ASCIIs[index]
+                })
+                index = index === 3 ? 0 : ++index;
+            }, 1000 / frequency);
+        }, 200)
+
+    }
+
+    render() {
+        return (
+            <div class="loading">
+                {this.state.loadingIcon}  Loading...
+            </div>
+        )
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timeoutQueue)
+        clearInterval(this.intervalQueue);
+    }
+}      
