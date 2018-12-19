c4b03f759ceab97f51d473ae728aad417abd0ebf
Harry Soer <gelo.soer@gmail.com>
2018-12-19 11:17:30 +0800

    Added styling for ads

--- src/components/Advertisement.js
+++ src/components/Advertisement.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Advertisement = (props) => {
     const source = `/ads/?r=${props.Id}`;
     return (
-        (<div style={{ width: '100%', height: '255px', textAlign: 'center' }}>
+        (<div class="ads">
             <p>But first, a word from our sponsors:</p>
-            <img src={source} />
+            <img class="ads-img" src={source} />
         </div>)
     )
 }
